2f1372cc1f1346acf95d11c21b754de9f5eec595
brandonruizmora <brandon_ruizmor@outlook.es>
2021-12-01 14:50:00 -0600

    middleware para las rutas

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,44 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Home from './components/home/HomePage'
 import FavPage from './components/favs/FavPage'
 import LoginPage from './components/login/LoginPage'
+import { connect } from 'react-redux'
 
-export default function Routes() {
+const PrivateRoute = ({ path, component, logged, ...rest }) => {
+
+    // using local storage
+    // let storage = localStorage.getItem('storage');
+    // storage = JSON.parse(storage)
+    // if (storage && storage.user) {
+    //     return <Route path={path} component={component} {...rest} />
+    // } else {
+    //     return <Redirect to="/login" {...rest} />
+    // }
+
+    // using redux
+    if (logged) {
+        return <Route path={path} component={component} {...rest} />
+    } else {
+        return <Redirect to="/login" {...rest} />
+    }
+}
+
+const Routes = ({ logged }) => {
     return (
         <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/favs" component={FavPage} />
+            <PrivateRoute exact path="/" component={Home} logged={logged} />
+            <PrivateRoute path="/favs" component={FavPage} logged={logged} />
             <Route path="/login" component={LoginPage} />
         </Switch>
     )
 }
+
+const mapStateToProps = ({ user: { logged } }) => {
+    return {
+        logged
+    }
+}
+
+
+export default connect(mapStateToProps)(Routes)
